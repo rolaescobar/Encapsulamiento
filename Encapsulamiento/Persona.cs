d3b67d4286cc1dc18a02335dc14153e5ed0f292e
Rolando José Garcia Escobar <rolaescobar@gmail.com>
2024-08-31 17:10:41 -0600

    Agregar archivos de proyecto.

---
+++ Encapsulamiento/Persona.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulamiento
+{
+    public class Persona
+    {
+
+        private string? nombre;
+        private int edad = 35;
+
+        public string GetNombre()
+        { 
+            
+        
+           return nombre ?? string.Empty;
+        
+        }
+
+        public void SetNombre(string nuevoNombre)
+        { 
+            nombre = nuevoNombre;
+        
+        }
+
+        public int GetEdad()
+        {
+            return edad;
+        }
+
+        public void Cumpleanos()
+        {
+
+            edad++;
+        }
+
+
+    }
+}
---
+++ Encapsulamiento/Program.cs
@@ -0,0 +1,15 @@
+﻿
+using Encapsulamiento;
+
+Persona persona = new Persona();
+
+persona.SetNombre("Rolando");
+Console.WriteLine("Nombre inicial: " + persona.GetNombre());
+
+Console.WriteLine("Edad inicial:" + persona.GetEdad());
+
+persona.Cumpleanos();
+Console.WriteLine("Edad despues del cumpleanos:"+persona.GetEdad());
+
+persona.SetNombre("Kevin");
+Console.WriteLine("Nombre despues del cambio a :"+persona.GetNombre());
